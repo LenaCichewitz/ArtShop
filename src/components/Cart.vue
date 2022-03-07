cc3a3ae2fad4222e3f53671889f756bb9b200f5d
Lena <cichewitz@gmail.com>
2022-03-07 22:20:51 +0100

    Zwischencommit

---
+++ src/components/Cart.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>
+                CART
+        </h1>
+    </div>
+</template>
+
+<script>
+// import CartEntity from "@/components/CartEntity";
+
+
+export default {
+    data() {
+    return {
+     page: "cart"
+    }
+  }, 
+};
+</script>
+
+<style scoped>
+
+</style>
